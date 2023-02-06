e3bbfa5ddc278df09497f6d6782dbe290c9c3ef4
Musa SAYAR <musasayar67@gmail.com>
2023-02-06 20:47:01 +0300

    Header eklendi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import {TodoProvider} from './Component/TodoProvider'
 import AddTodo from './Component/AddTodo'
 import TodoList from './Component/TodoList'
+import Header from './Component/Header';
 function App() {
   return (
+
+    <>
+   
 <div className="container flex flex-column place-items-center phone:w-95 sm:w-98 md:w-120 mt-10 md:mt-16 border border-gray-400 rounded-lg p-2 sm:p-6 md:p-8">
+<Header/>
 <TodoProvider>
     <AddTodo/>
     <TodoList/>
 </TodoProvider>
 </div>
+
+</>
   );
 }
 
---
+++ src/Component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className=''>
+      <h1 className=' rounded-xl phone:w-95  md:w-98 p-4 md:p-6 text-center text-xl sm:text-2xl md:text-3xl font-bold text-black'>Todo App</h1>
+    </div>
+  )
+}
+
+export default Header
